18f8cc04bc96cd1cdcc770c15ae0c9fba5c09ad0
Larissor <larissa12.passos@gmail.com>
2024-07-05 15:55:35 -0300

    Commit

--- view/DetalheChamado/index.php
+++ view/DetalheChamado/index.php
@@ -64,6 +64,26 @@ require_once("../../config/connection.php");
                             <div class="panel-body">
                                 <section id="lbldetalhe">
                                 <ul class="chat">
+
+                                <li class="left clearfix">
+                <span class="chat-img pull-left">
+                    <img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar"
+                            class="img-circle"/>
+                </span>
+
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">Jack Sparrow</strong>
+                    <small class="pull-right text-muted">
+                        <i class="fa fa-clock-o fa-fw"></i> 12 mins ago
+                    </small>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                    ornare dolor, quis ullamcorper ligula sodales.
+                </p>
+               </div>
+            </li>
                                
                                 </ul>
                             </section>
